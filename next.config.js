fb546d8232a0205dc23acd8f24b307756b49b443
sajjadakbariai <sajjadakbari.ai@gmail.com>
2025-04-30 01:18:19 +0330

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          }
+        ]
+      }
+    ]
+  }
+}
